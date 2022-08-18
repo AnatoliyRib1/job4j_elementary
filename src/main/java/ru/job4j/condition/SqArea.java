418a7d1e8f7fab9fa58d0e9bc2f727a2d6fa68a4
AnatoliyRib1 <anatoliy.rib@yandex.ru>
2022-08-18 11:58:18 +0300

    Стороны прямоугольника. [#137735]

---
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class SqArea {
+    public static double square(double p, double k) {
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        double s = l * h;
+        return h;
+
+    }
+
+    public static void main(String[] args) {
+        double result1 = SqArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+    }
+}
+/*как я выяснил, простым путем более одного значения из метода square java вернуть не дает, а потому объявление и
+расчет переменных l и s  в целом внутри метода не имеют значения. Может просто я неправильно понял смысл */
