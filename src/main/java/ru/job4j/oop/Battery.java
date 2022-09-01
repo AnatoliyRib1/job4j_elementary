d926a824ea03abd2d1bd0b24d3d9ea4393e80bc4
AnatoliyRib1 <anatoliy.rib@yandex.ru>
2022-09-01 10:59:16 +0300

    Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,24 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void exchange(Battery another) {
+        another.load = this.load + another.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery charger = new Battery(5);
+        Battery another = new Battery(15);
+    }
+
+}
