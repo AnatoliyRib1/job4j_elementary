3c00b366c964b87ba7d72e180c322770b1a233ed
AnatoliyRib1 <anatoliy.rib@yandex.ru>
2022-09-01 09:10:47 +0300

    Вызов метода с возвращаемым типом. [#167387 #338233]

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -3,11 +3,10 @@ package ru.job4j.oop;
 public class DummyDic {
     public String engToRus(String eng) {
         return   "Неизвестное слово. " + eng;
-        }
+    }
 
     public static void main(String[] args) {
         DummyDic translate = new DummyDic();
         System.out.println(translate.engToRus("Hello"));
-
     }
 }
