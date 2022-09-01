da0188d0fa9d75db64c1c82286ba5559c5d8d0b0
AnatoliyRib1 <anatoliy.rib@yandex.ru>
2022-09-01 10:22:17 +0300

    Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error err = new Error(true, 1, "Good");
+        error.printInfo();
+        err.printInfo();
+    }
+}
